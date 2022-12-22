b75a09cc07ec6d59adc8df1f64ebc4de61c86d9f
Nasilele1 <jasperkatondo@gmail.com>
2022-12-22 06:29:33 -0800

    hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * _strncat - function that concatenates two strings
  * @dest: copy to
@@ -7,7 +7,8 @@
  * @n: any integer
  * Return: string
  */
-char *_strncat(char *dest, char *src, int)
+char *_strncat(char *dest, char *src, int n)
 {
 	strncat(*dest, *src, n);
 	return (dest);
+}
