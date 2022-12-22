4c76ab9a7cf19f299e7db09e6c7f04b708abd3c5
Nasilele1 <jasperkatondo@gmail.com>
2022-12-22 04:12:14 -0800

    function that concatenates two strings with n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: any integer
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int)
+{
+	strncat(*dest, *src, n);
+	return (dest);
