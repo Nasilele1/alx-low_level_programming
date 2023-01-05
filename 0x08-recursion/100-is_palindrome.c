26be15a86cbd46924e1a45e18ceb438031f8bf19
Nasilele1 <jasperkatondo@gmail.com>
2023-01-05 06:58:57 -0800

    A function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
-/***
+/**
  * palind2 - obtains length of a
  * @a: string
  * @l: integer to count length
  *
  * Return: On success 1.
- * On error, -1 iss returned, and errno is set appropriately
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int palind2(char *a, int l)
 {
 	if (*a == 0)
 	{
-		return (1);
+		return (l - 1);
 	}
 	return (palind2(a + 1, l + 1));
 }
@@ -28,20 +28,19 @@ int palind3(char *a, int l)
 {
 	if (*a != *(a + 1))
 	{
-		return (1);
+		return (0);
 	}
 	else if (*a == 0)
 	{
-		return (0);
+		return (1);
 	}
 	return (palind3(a + 1, l - 2));
 }
 /**
- * is_palindrome - checks if a string is a palindrome
+ * is_palindrome - checks if the string is a palindrome
  * @s: string to evaluate
  *
- * Return:
- * On success 1.
+ * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int is_palindrome(char *s)
