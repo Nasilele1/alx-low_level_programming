42cf1b8fc9110901f922323f522116d2f5165dd5
Nasilele1 <jasperkatondo@gmail.com>
2023-01-09 04:30:21 -0800

    static library libmy.a containing all the functions

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - function to locate a char in string
+ * @s: string to search
+ * @c: character to search for
+ *
+ * Return: c if found and NULL if not
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+	return (0);
+}
