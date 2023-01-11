f77a6e7d518981f23136647aba0e30b0ff653413
Nasilele1 <jasperkatondo@gmail.com>
2023-01-11 08:09:14 -0800

    a function that concatenates two strings.

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,9 +1,12 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <string.h>
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int _strlen(char *s);
+char *str_concat(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
