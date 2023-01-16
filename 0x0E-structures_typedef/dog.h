f366b4fe20cb144fb9b44d67681aeca283d3454c
Nasilele1 <jasperkatondo@gmail.com>
2023-01-16 09:27:35 -0800

    A new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * File: dog.h
+ * Auth: Brennan D Baraban
+ * Desc: Header filr tat defines a new type struct dog.
+ */
+
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+#endif
