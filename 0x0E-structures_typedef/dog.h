d6d13556869308ad588e9bb1e25bde2b7f9f82b4
Nasilele1 <jasperkatondo@gmail.com>
2023-01-16 09:48:29 -0800

    A new type struct dog with elements name, age and owner

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,14 @@
 #define DOG_H
 
 /**
+ * Main - check the code
  * File: dog.h
  * Auth: Brennan D Baraban
  * Desc: Header filr tat defines a new type struct dog.
  */
 
 /**
- * struct dog - A new type describing a dog.
+ * struct dog - struct dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
